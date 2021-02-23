f97b723f3bf15cabe7b26baf42409ac7eee0c001
toshinari <ztl40.takagi.toshina@gmail.com>
2021-02-23 23:49:58 +0900

    add プレイ画面

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function showSum(event) {
-    var num1 = 1;
-    var num2 = 2;
-    var sum = num1 + num2;
-    alert(sum);
+function selectText() {
+    //問題文の配列
+    const textList= [
+        'nike'  ,
+        'mizuno',
+        'adidas',
+        'puma'  ,
+        'asics'
+    ];
+    //配列のインデックスを乱数で取得
+    const arrayIndex = Math.floor(Math.random() * textList.length);
+    //問題文の要素を取得
+    const questionText = document.getElementById("questionText");
+    //要素のinnerTextに配列の文字列を入れる
+    questionText.innerHTML= textList[arrayIndex]; 
 }
