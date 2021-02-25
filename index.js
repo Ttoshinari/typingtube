7501737751a6e96ec857d508c171067a70f2b70e
toshinari <ztl40.takagi.toshina@gmail.com>
2021-02-25 23:45:45 +0900

    edit html & js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function selectText() {
+function init() {
     //問題文の配列
     const textList= [
         'nike'  ,
@@ -7,10 +7,25 @@ function selectText() {
         'puma'  ,
         'asics'
     ];
+    //問題文の要素を取得
+    const questionTextElement = document.getElementById("questionText");  
+    const inputText           = typingForm.inputText.value;
+    const resultText          = document.getElementById("resultText");
+    console.log(questionText);
+
+    if(inputText == ''){
+        resultText.innerHTML = '文字を入力してください';
+    }else if(inputText == questionTextElement.innerHTML){
+        resultText.innerHTML = '正解';
+    }else {
+        resultText.innerHTML = '残念';
+    }
+
     //配列のインデックスを乱数で取得
     const arrayIndex = Math.floor(Math.random() * textList.length);
-    //問題文の要素を取得
-    const questionText = document.getElementById("questionText");
+    console.log(arrayIndex);
     //要素のinnerTextに配列の文字列を入れる
-    questionText.innerHTML= textList[arrayIndex]; 
+    questionTextElement.innerHTML= textList[arrayIndex];
+    document.typingForm.reset();
+
 }
